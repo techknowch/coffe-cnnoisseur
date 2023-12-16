7b4af327b975367aeec2a894c3727bef1d2dc77f
techknowch <techknowch@protonmail.com>
2023-12-16 13:22:03 +0500

    Ali

--- pages/hello.tsx
+++ pages/hello.tsx
@@ -1,5 +1,7 @@
+import styles from '../styles/Hello.module.css';
+
 const Hello = () => {
-    return <div>Hello World</div>;
+    return <div className={styles.container}>Hello World</div>;
 };
 
 export default Hello;
